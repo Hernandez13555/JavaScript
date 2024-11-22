57b06b5b3cb3b88e32a5fd08b090e0db3135f6f7
Reinel David Hernandez <davidhernandez3884@gmail.com>
2024-11-22 11:28:13 -0500

    JS

--- ciclos.js
+++ ciclos.js
@@ -10,6 +10,31 @@
 // } while (falso);
 
 
-for (let i = 2; i <= 40; i++) {
-    console.log(i++);
+// for (let i = 2; i <= 40; i++) {
+//     console.log(i++);
+// }
+
+
+
+
+let contador = 0;
+let num = 2;
+
+for (let contador = 0; contador < 5; num++) {
+    let esPrimo = true;
+
+    // Verificar si num es primo
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            esPrimo = false;
+            break;
+        }
+    }
+
+    // Si es primo, imprimir el número y aumentar el contador
+    if (esPrimo) {
+        console.log(num);
+        contador++;
+    }
 }
+
